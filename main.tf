d364ad83da95bd16e7ee716a5cb7537b2b46e4ea
umasujatha9643 <umasujatha9643@gmail.com>
2025-12-12 22:43:42 +0530

    Remove Google provider configuration

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
 resource "google_compute_network" "demo_vpc" {
     name = "demo-vpc"
     auto_create_subnetworks = true
